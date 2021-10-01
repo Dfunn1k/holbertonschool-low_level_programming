d7149e6fe87926b9bd8b1b1f0e1f1c8eb4c601f5
BigDany1792 <dany1792dota@gmail.com>
2021-10-01 01:46:32 +0000

    added my task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the text
+ * void
+ * Description: My function display the text added
+ * My function use the default function puts for display the text
+ * Return: value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
