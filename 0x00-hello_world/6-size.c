c8c8d398ff822b8eb14472e1f5d0ffb686257163
BigDany1792 <dany1792dota@gmail.com>
2021-10-01 02:46:13 +0000

    added task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s)", sizeof(float));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
