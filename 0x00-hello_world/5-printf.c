b8e33c0f921939264a94f79afdfb4c28e6e9d2dc
BigDany1792 <dany1792dota@gmail.com>
2021-10-01 01:57:11 +0000

    added my task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - print the text
+  * void
+  * Description: My function display the text added
+  * My function use the default function printf to display the text
+  * Return: value 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
