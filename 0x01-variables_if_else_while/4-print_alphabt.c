3be2a6380eba64beced69d621c8d391122eae717
BigDany1792 <dany1792dota@gmail.com>
2021-10-04 03:31:40 +0000

    added exercise 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *main - display alphabet
+ *var1 : ascii 97
+ *Description : my function display alphabet in lowercase except q and e
+ *My function display the alphabet in lower case excet q and e
+ *Return: value 0
+ */
+int main(void)
+{
+	int var1;
+
+	var1 = 97;
+
+	while (var1 < 123)
+	{
+		if (var1 != 101 || var1 != 113)
+		{
+			putchar(var1);
+			var1++;
+		}
+		else
+		{
+		}
+	}
+putchar(10);
+return (0);
+}
