38a33b2637ae13acec4657489622e3885576c819
BigDany1792 <dany1792dota@gmail.com>
2021-10-06 23:21:54 +0000

    fix error

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int var1, var2;
+
+	var1 = ;
+	var2 = 48;
+
+	while (var1 < 58)
+	{
+		if (var1 != var2)
+		{
+			putchar(var1);
+			putchar(var2);
+			var2++;
+
+		}
+		else
+		{
+			var1++;
+		}
+		
+	}
+
+return (0);
+}
