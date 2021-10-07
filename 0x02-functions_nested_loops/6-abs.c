b90b44613046f962cf59deac5adf80d1a1d398a9
BigDany1792 <dany1792dota@gmail.com>
2021-10-07 04:41:44 +0000

    return the absolute value of the number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_abs- return the absolute value of the number
+ *@c: a number
+ *Description: Display the absoiute value of the number
+ *with help "abs" function
+ *Return: 0 value
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
