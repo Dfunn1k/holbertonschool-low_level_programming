2015ae24612e04747756ce43f5f6861f9bdb6639
BigDany1792 <dany1792dota@gmail.com>
2021-10-07 02:20:41 +0000

    added exercises 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int cont1, cont2;
+
+	cont1 = 97;
+
+	cont2 = 0;
+
+	while (cont2 <= 9)
+	{
+		while (cont1 <= 122)
+		{
+			_putchar(cont1);
+			cont1++;
+		}
+		_putchar('\n');	
+		cont1 = 97;
+	cont2++;	
+	}
+
+}
