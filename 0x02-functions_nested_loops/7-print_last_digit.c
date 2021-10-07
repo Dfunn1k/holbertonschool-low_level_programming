eb5c78d8cd3671510338a29349dd380be13405c2
BigDany1792 <dany1792dota@gmail.com>
2021-10-07 04:57:15 +0000

    display the last digit of the number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_last_digit - return the last digit
+ *@n: a number
+ *Description: Display the last digit of the numbers
+ *using the module
+ *Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int ld;
+	ld = n % 10;
+	return (ld);
+}
