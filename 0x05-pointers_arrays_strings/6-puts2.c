5329bcd3b577bc56a9f2c780f0dc258eede99ab4
BigDany1792 <dany1792dota@gmail.com>
2021-10-18 05:06:01 +0000

    added exercise 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,14 @@ void puts2(char *str)
 
 	while (str[i] != 0)
 	{
-		_putchar(str[i]);
-		i++;
-		_putchar(10);
+		if (i == 0 || i % 2 == 0)
+		{
+			_putchar(str[i]);
+			i++;
+		}else
+		{
+			i++;
+		}
 	}
+	_putchar(10);
 }
