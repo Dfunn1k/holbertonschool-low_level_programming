7f05b7a54d3ba6fc3379c9368104615e2604b4ca
BigDany1792 <dany1792dota@gmail.com>
2021-10-18 23:33:51 +0000

    added exercise 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_array - display n characters of an array
+ *@a: pointer to array
+ *@n: integer
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d",a[i]);
+		if (i == n)
+			break;
+		printf(", ");
+		i++;
+	}
+	printf("\n");
+}
