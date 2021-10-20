f641355c6f85070ba67153ab7c245f889830c00b
BigDany1792 <dany1792dota@gmail.com>
2021-10-20 18:40:24 +0000

    added exercise 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *factorial - print the factorial of a given number
+ *@n: integer number
+ *
+ *Return: factorial de n
+ */
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		if (n == 1)
+		{
+			return (1);
+		} else
+		{
+			return (n * factorial(n - 1));
+		}
+
+	} else
+	{
+		return (-1);
+	}
+}
