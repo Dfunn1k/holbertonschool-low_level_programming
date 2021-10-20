d1b9f4b6aff8efe6e633be1bebe26edf59bae24e
BigDany1792 <dany1792dota@gmail.com>
2021-10-20 18:55:26 +0000

    added exercise 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - print a string in reverse with recursion
+ *@s: pointer to a string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+	} else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
