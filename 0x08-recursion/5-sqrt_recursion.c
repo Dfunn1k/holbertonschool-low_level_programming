98ed62cf43f132f7424026b30478a33f98edc17a
BigDany1792 <dany1792dota@gmail.com>
2021-10-21 21:31:42 +0000

    added exercise 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 int fun_srqt(int n, int a);
 
 /**
- *_pow_recursion - x raised to the power of y
+ *fun_srqt - x raised to the power of y
  *@n: integer number
  *@a: integer number
  *
@@ -15,8 +15,8 @@ int fun_srqt(int n, int a)
 	else if (n < 0)
 		return (-1);
 
-	n-=a;
-        return (fun_srqt(n, a + 2));
+	n -= a;
+	return (fun_srqt(n, a + 2));
 }
 
 /**
@@ -30,7 +30,6 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 		return (0);
 	else if (n < 0)
-        	return (-1);
+		return (-1);
 	return (fun_srqt(n, 1));
-		
 }
