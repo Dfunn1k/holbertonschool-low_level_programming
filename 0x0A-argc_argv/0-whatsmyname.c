c877bf35c4535dc5947f5cac4ccd37776aaf1c01
BigDany1792 <dany1792dota@gmail.com>
2021-10-25 04:53:32 +0000

    added exercise 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mynameis - a program that prints its name
+ *@argc: name of the function
+ *@argv: array of the strings
+ *
+ *Return: 0(function success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+return (0);
+}
