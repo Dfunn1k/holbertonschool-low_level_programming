07335c4d224f0b2a712f152868a627d56ca34420
BigDany1792 <dany1792dota@gmail.com>
2021-10-25 04:06:18 +0000

    added exercise 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_pow_recursion - x raised to the power of y
+ *@x: integer number
+ *@y: integer number
+ *
+ *Return: x raised to the power of y, -1 if y < 0
+ */
+int prime(int n, int c)
+{
+	if (n <= 1)
+		return (1);
+	if (n %c == 0)
+		return (0);
+	if (n % c != 0)
+		return (prime(n, c - 1);			
+}
+int is_prime_number(int n)
+{
+ad
+
+}
