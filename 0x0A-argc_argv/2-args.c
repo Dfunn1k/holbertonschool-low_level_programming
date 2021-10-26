f80ce6fc1bbfd034e1765320117d2b2ed52eae8b
BigDany1792 <dany1792dota@gmail.com>
2021-10-26 02:38:24 +0000

    added exercise 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - display all arguments it receives
+ *@argc: contain the number of arguments
+ *@argv: an array of the arguments
+ *
+ *Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	putchar(10);
+return (0);
+}
