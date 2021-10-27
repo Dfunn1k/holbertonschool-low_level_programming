ca3a00f7139ec6f17a437ddded8bf68b7ba2e4fc
BigDany1792 <dany1792dota@gmail.com>
2021-10-27 03:12:44 +0000

    added git ignore

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include:W <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - Function to obtain string length
+ * @s: Parameter
+ * Return: Return value
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+	{
+		return (0);
+	}
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+/**     
+ * _strdup - Function to reserve memory and copy string
+ * @str: Parameter
+ * Return: Return value
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *p = (char *)malloc(_strlen(str) + 1);
+
+	if (str == NULL || p == NULL)
+	{
+		return (NULL);
+	}
+	while (i < _strlen(str))
+	{
+		*(p + i) = *(str + i);
+		i++;
+	}
+	return (p);
+}
