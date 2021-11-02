b50cfebe05f237901982dd6f6c31d48f643bedf5
BigDany1792 <dany1792dota@gmail.com>
2021-11-02 15:52:01 -0500

    added exercise 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ *struct dog - struct of my dog
+ *@name: First member
+ *@age: Second member
+ *@owner: thir member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
