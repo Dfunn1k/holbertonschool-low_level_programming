52bfed065d2ec02dfb3069082ffce45692845391
BigDany1792 <dany1792dota@gmail.com>
2021-11-02 22:57:00 -0500

    added file with the function new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,6 +19,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	dog->name = malloc((strlen(name) + 1));
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		return (NULL);
+	}
+
+	dog->owner = malloc((strlen(name) + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		return (NULL);
+	}
+
 	strcpy(dog->name, name);
 	dog->age = age;
 	strcpy(dog->owner, owner);
