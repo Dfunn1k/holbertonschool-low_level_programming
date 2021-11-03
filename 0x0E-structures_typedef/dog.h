170fba7724c26b9ae8b4ac5f731ba9ad79d8a706
BigDany1792 <dany1792dota@gmail.com>
2021-11-02 21:59:11 -0500

    update header file with typedef to struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,13 @@
  *@age: Second member
  *@owner: thir member
  */
-typedef struct dog dog_t;
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
