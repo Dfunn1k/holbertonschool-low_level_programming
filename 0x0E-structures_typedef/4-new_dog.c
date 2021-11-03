fa5cd7907c30df1d6fec7c8427bf5d7952c35af4
BigDany1792 <dany1792dota@gmail.com>
2021-11-02 23:09:54 -0500

    fix error

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,9 +10,8 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *dog = malloc(sizeof(dog_t));
 
-	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 	{
 		return (NULL);
