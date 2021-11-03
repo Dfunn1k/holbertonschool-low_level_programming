c64fbc565ca30e9cdad41bdca8e07d29c3d3818d
BigDany1792 <dany1792dota@gmail.com>
2021-11-02 23:28:56 -0500

    added file with the function freedog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *free_dog - function that frees dogs
+ *@d: pointer to structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
