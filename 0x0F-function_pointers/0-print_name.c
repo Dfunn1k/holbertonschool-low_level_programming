caadc77ff9afe0e11c53f2beb9bf101fe0f03146
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 19:14:02 -0500

    added file with function that print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,15 @@
-#include 
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ *print_name - Function that prints a name
+ *@name: string of character
+ *@f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	if (f == NULL)
+		return;
+	(f)(name);
+}
