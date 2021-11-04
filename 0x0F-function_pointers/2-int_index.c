8b07bf58e56206018711afb48317846931649988
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 21:18:11 -0500

    added file with the function index-int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *int_index - function that searches for an integer
+ *@array: pointer to array
+ *@size: size of the array
+ *@cmp: pointer to function
+ *Return: the index of the first element matched
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 1; i < size; i++)
+	{
+		if ((cmp)(array[i]) == 1)
+			return (i);
+	}
+	return (-1);
+}
