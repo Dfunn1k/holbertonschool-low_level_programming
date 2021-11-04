360028fb08f295a777c8a1aa0c7f89b4ce6d7683
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 20:51:42 -0500

    added file with the function array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ *array_iterator - that executes a function given
+ *@array: pointer to array
+ *@size: size of the array
+ *@action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
+	for(i = 0; i < size; i++)
+	{
+		(action)(array[i]);
+	}
+}
