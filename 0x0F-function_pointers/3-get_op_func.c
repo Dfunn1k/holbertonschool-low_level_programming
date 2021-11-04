cdd6eccecfdb41a318c69146a25de88eea01a8fe
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 23:02:33 -0500

    added file with the function get other function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ *get_op_func - Function pointer to function
+ *@s: pointer to char
+ *Return: pointer to the function that corresponds to the operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (i <= 4)
+	{
+		if (*s == *ops[i].op)
+		{
+			return (*(ops[i]).f);
+		}
+		i++;
+	}
+	return (NULL);
+}
