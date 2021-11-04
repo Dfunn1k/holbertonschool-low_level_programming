98b8b7e5c07ddea3f078eb1c52c04640f6407539
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 20:54:40 -0500

    fix betty style

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,13 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		(action)(array[i]);
 	}
