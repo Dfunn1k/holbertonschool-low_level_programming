4cd955c5a0d592184354718ead5af85f73a23360
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 23:52:47 -0500

    fix error

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
 #ifndef FUNCT_POINTERS
 #define FUNCT_POINTERS
-#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
