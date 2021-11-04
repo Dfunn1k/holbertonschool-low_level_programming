081d844fd35983c43db3d5139956ee7aeba11723
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 23:04:03 -0500

    added main file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ *main - Function main
+ *@argc: number of arguments
+ *@argv: stored arguments
+ *Return: function selected
+ */
+int main(int argc, char *argv[])
+{
+	int b, c;
+	char *a;
+
+	if (argc == 4)
+	{
+		a = argv[2];
+		b = atoi(argv[1]);
+		c = atoi(argv[3]);
+		if (get_op_func(a) == NULL || argv[2][1] != '\0')
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		return (printf("%d\n", get_op_func(a)(b, c)));
+	}
+	printf("Error\n");
+	exit(98);
+}
