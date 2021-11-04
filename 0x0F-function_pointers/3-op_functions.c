ef4c6f52d236c77186f58fa0b8ad934499c86a27
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 23:02:59 -0500

    added file with the operator functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ *op_add - return the sum of a and b
+ *@a: integer number
+ *@b: integer number
+ *Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - return the difference
+ *@a: integer number
+ *@b: integer number
+ *Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - return the product
+ *@a: integer number
+ *@b: integer number
+ *Return: the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - return the result of the division
+ *@a: integer number
+ *@b: integer number
+ *Return: the result of the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - return the module
+ *@a: integer number
+ *@b: integer number
+ *Return: the remainder of the division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
