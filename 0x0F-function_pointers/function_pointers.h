fbc4d2a785d45c242c55689202a3a8f855639bad
BigDany1792 <dany1792dota@gmail.com>
2021-11-03 20:51:19 -0500

    added stddef.h and prototype by function array_iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
+#include <stddef.h>
 #ifndef FUNCT_POINTERS
 #define FUNCT_POINTERS
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
