10ea02b8dc76404378152491526204c0ea86cbba
BigDany1792 <dany1792dota@gmail.com>
2021-11-04 20:39:45 -0500

    added exercise advanced

---
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -0,0 +1,17 @@
+global main
+section	.text
+
+main:
+	mov eax,0x4
+	mov ebx,1
+	mov ecx,message
+	mov edx,message_length
+	int 0x80
+
+	mov eax,0x1
+	mov ebx,0
+	int 0x80
+
+section	.data
+message	db "Hello, World",0xA
+message_length equ $-message
