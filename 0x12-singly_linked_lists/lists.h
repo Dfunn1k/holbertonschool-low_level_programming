06b9d88474e672ba9b823dafa04d7c6be5e214b6
BigDany1792 <dany1792dota@gmail.com>
2021-11-11 22:28:43 -0500

    chore: new header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef _LINKED_LIST_
+#define _LINKED_LIST_
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+#endif
