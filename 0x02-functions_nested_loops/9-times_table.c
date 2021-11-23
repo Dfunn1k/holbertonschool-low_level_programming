c9054bcff29bd228c3bd387108abe77c985178de
BigDany1792 <dany1792dota@gmail.com>
2021-11-23 14:17:44 -0500

    feat: added function that prints the 9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *times_table - function that prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int num, count, last_digit, first_digit, mul;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (count = 0; count <= 9; count++)
+		{
+			mul = num * count;
+			first_digit = mul / 10;
+			last_digit = mul % 10;
+			mul += 48;
+
+			if (first_digit > 0)
+			{
+				_putchar(first_digit + 48);
+				_putchar(last_digit + 48);
+			}
+			if (first_digit <= 0)
+			{
+				_putchar(32);
+				_putchar(mul);
+			}
+			if (count != 9)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+	_putchar(10);
+	}
+}
