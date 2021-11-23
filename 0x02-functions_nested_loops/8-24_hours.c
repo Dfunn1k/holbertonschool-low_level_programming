61c8d496ae27458f181a4111c386d7da3ade1e1e
BigDany1792 <dany1792dota@gmail.com>
2021-11-22 23:26:48 -0500

    feat: added function that prints all hours of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,50 +1,40 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
+ *jack_bauer - function that prints every minute of the day of jack bauer
  */
 
 void jack_bauer(void)
 {
-	int x, y, z, w;
+	int w, x, y, z;
 
     w = 48;
-    z = 48;
-    y = 48;
     x = 48;
-	while(x < 51)
+    y = 48;
+    z = 48;
+
+	while (w < 51)
 	{
-		if(x == 50 && y == 52)
+		while (x < 52)
 		{
-			break;
-		}
-		else
-		{			
-			while(y < 58)
+			while (y < 54)
 			{
-				while(z < 54)
-				{	
-				    while(w < 58)
-				    {
-		       			 _putchar(x);
-		       			 _putchar(y);
-		       			 _putchar(58);
-		       			 _putchar(z);
-		       			 _putchar(w);
-		       			 _putchar(10);
-		       			 w++; 
-				    }
-				w = 48;
-				z++;	
+				while (z < 58)
+				{
+					_putchar(w);
+					_putchar(x);
+					_putchar(58);
+					_putchar(y);
+					_putchar(z);
+					_putchar(10);
+					z++;
 				}
 			z = 48;
 			y++;
 			}
 		y = 48;
-		x++;	
+		x++;
 		}
-	}
-}
+	x = 48;
+	w++;	
+	}	
+}		
