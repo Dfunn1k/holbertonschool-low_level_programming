ef383b3a5ab08ba07c7565c2834402fc422382e9
BigDany1792 <dany1792dota@gmail.com>
2021-12-06 16:01:47 -0500

    added functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - Function to print lower alphabet
+ *@n: Parameter
+ * Return: Return value "n"
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+
+}
