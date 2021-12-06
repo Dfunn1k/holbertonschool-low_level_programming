c2d561803404cfd2b4f1a8263ae5eb97b1b2e0a9
BigDany1792 <dany1792dota@gmail.com>
2021-12-06 15:58:15 -0500

    added function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - Function to return 1 if C is a lower and 0 if C is upper
+ *@a: Parameter
+ *@b: Parameter
+ *Return: value "n"
+ */
+int add(int a, int b)
+{
+	int r;
+
+	r = (a + b);
+	return (r);
+}
