97d4e9fae858878862c9b9f7906e1208fdd413a7
BigDany1792 <dany1792dota@gmail.com>
2021-12-06 16:07:47 -0500

    style: reduce coding

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -40,9 +40,7 @@ void print_times_table(int n)
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
 				} else
-				{
 					_putchar(c + '0');
-				}
 			}
 			_putchar('\n');
 		}
