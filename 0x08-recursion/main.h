f49e9d146ef2bc0b038119a5f418e0cb86b475d4
BigDany1792 <dany1792dota@gmail.com>
2021-12-07 19:26:55 -0500

    added prototypes in header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,3 +5,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pwd_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int wildcmp(char *s1, char *s2);
+int is_palindrome(char *s);
+int is_prime_number(int n);
