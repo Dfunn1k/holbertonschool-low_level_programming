7ccf9eb77727b088afaec65edded9c7255ef8fa7
BigDany1792 <dany1792dota@gmail.com>
2022-02-10 13:24:31 -0500

    ci: added function that print wheteher the number stored in the variable n is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The variable n is reassigned of valor
+ * Return: success(0)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
