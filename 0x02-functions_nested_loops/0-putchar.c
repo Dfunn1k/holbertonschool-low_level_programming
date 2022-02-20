7677cf926c12274da9b60b7ff0a05ed6553984f3
BigDany1792 <dany1792dota@gmail.com>
2022-02-20 11:17:31 -0500

    ci: added function that prints '_putchar', followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print '_putchar'
+ * Return: success(0)
+ */
+
+int main(void)
+{
+	_putchar(95);
+	_putchar('p');
+	_putchar(117);
+	_putchar('t');
+	_putchar(99);
+	_putchar('h');
+	_putchar(97);
+	_putchar('r');
+	_putchar(10);
+return (0);
+}
