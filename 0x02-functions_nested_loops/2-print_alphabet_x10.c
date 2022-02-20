738f232a2bccb873163d63a0ce738ec6a167e0d7
BigDany1792 <dany1792dota@gmail.com>
2022-02-20 11:19:37 -0500

    ci: added function that prints 10 times the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - display the alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int letter = 97, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (letter < 123)
+		{
+			_putchar(letter);
+			letter++;
+		}
+		letter = 97;
+		_putchar(10);
+	}
+	_putchar(10);
+}
