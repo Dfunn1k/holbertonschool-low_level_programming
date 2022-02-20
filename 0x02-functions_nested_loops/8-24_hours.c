e5210abd53dbc41dd1c0b42e3548378b47c64c23
BigDany1792 <dany1792dota@gmail.com>
2022-02-20 13:49:55 -0500

    ci: added function that prints every minute of the day, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ */
+
+void jack_bauer(void)
+{
+	int dh = 48, uh = 48, dm = 48, um = 48;
+
+	while (dh < 51)
+	{
+		while (dh == 50 && uh != 52)
+		{
+			while (dm < 54)
+			{
+				while (um < 58)
+				{
+					_putchar(dh);
+					_putchar(uh);
+					_putchar(':');
+					_putchar(dm);
+					_putchar(um);
+					_putchar(10);
+					um++;
+				}
+				um = 48;
+				dm++;
+			}
+			um = 48;
+			dm = 48;
+			uh++;
+		}
+		um = 48;
+		dm = 48;
+		uh = 48;
+		dh++;
+	}
+}
