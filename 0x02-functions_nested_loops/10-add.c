f82437d27aae057ffaa19142a48ba751f204f4d4
BigDany1792 <dany1792dota@gmail.com>
2022-02-24 19:30:04 -0500

    ci: added

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * 
- * 
+ * add - add two integer
+ * @x: integer
+ * @y: integer
+ * Return: the sum
  */
 
 int add(int x, int y)
@@ -10,6 +12,6 @@ int add(int x, int y)
 	int sum;
 
 	sum = x + y;
-	
+
 return (sum);
-} 
+}
