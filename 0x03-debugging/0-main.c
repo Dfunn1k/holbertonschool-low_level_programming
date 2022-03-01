2c07844b6b330a3152ccd36ce1f9f0d1f2025e2f
BigDany1792 <dany1792dota@gmail.com>
2022-03-01 13:11:00 -0500

    ci: added function main for test with case 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test case of positive_or_negative with zero
+ * Return: success(0)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+return (0);
+}
