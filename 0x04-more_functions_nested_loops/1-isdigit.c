6a67681abed9cd0c91e0199bead63e137a3c5eb2
BigDany1792 <dany1792dota@gmail.com>
2022-03-08 12:54:19 -0500

    ci: added function that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit(0 trough 9)
+ * @c: character
+ * Return: if is a digit return 1, otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+
+return (0);
+}
