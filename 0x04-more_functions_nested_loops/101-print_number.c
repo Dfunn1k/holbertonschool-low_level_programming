b0b17e4744edd3a33a629fa2ff34dfdbacd75e6c
BigDany1792 <dany1792dota@gmail.com>
2022-03-10 16:58:42 -0500

    ci: added function that printed an integer only use _putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer that will be printed
+ */
+
+void print_number(int n)
+{
+	int tmp, tmp2, digits, qcient, i, divisor;
+
+	digits = 0;
+	divisor = 1;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+
+	tmp = n;
+
+	while (tmp > 0)
+	{
+		tmp /= 10;
+		digits++;
+	}
+
+	tmp2 = digits;
+
+	while (tmp2 > 1)
+	{
+		divisor *= 10;
+		tmp2--;
+	}
+
+	if (digits == 1 || n == 0)
+		_putchar(n + 48);
+	else
+	{
+		for (i = 0; i < digits; divisor /= 10)
+		{
+			qcient = n / divisor;
+			n %= divisor;
+			_putchar(qcient + 48);
+			i++;
+		}
+	}
+}
