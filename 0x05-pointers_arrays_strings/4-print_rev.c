4f163583912936bd19815670d3051089d618c060
BigDany1792 <dany1792dota@gmail.com>
2022-03-12 18:03:56 -0500

    style: fix betty style

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,14 @@
 
 /**
  * print_rev - prints a string in revers, following by a new line
- * @s: pointer to string 
+ * @s: pointer to string
  */
 
 void print_rev(char *s)
 {
 	while (*s != '\0')
 		s++;
-	
+
 	s--;
 
 	while (*s != '\0')
