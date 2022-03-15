7e0f4fafd64aa39f68769a6f5cd39497bf7ddef4
BigDany1792 <dany1792dota@gmail.com>
2022-03-15 16:50:39 -0500

    ci: added function that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+#define MATCH 1
+#define NOMATCH 0
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: pointer to string
+ */
+
+char *cap_string(char *str)
+{
+	char *tmpp, *ep;
+	char exceptions[] = " \t\n,;.?\"(){}";
+	int state;
+
+	tmpp = str;
+
+	while (*str != '\0')
+	{
+		state = NOMATCH;
+		ep = exceptions;
+
+		while (*ep != '\0')
+		{
+			if (*str == *ep)
+				state = MATCH;
+			ep++;
+		}
+
+		str++;
+
+		if (state == MATCH && *str < 123 && *str > 96)
+			*str -= 32;
+	}
+
+	return (tmpp);
+}
