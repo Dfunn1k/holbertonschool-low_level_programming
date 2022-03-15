bb8bb160e260c8a511ae58bb6113397fa905dc68
BigDany1792 <dany1792dota@gmail.com>
2022-03-15 01:18:54 -0500

    ci: Added function that compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: This is the first string to be compared
+ * @s2: This is the second string to be compared
+ * Return:
+ * if Return value < 0 then it indicates str1 is less than str2.
+ * if Return value > 0 then it indicates str2 is less than str1.
+ * if Return value = 0 then it indicates str1 is equal to str2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	x = 0;
+
+	while (s1[x] != '\0' || s2[x] != '\0')
+	{
+		if (s1[x] == s2[x])
+			x++;
+		else if (s1[x] > s2[x])
+			return (1);
+		else
+			return (-1);
+	}
+
+	return (0);
+}
