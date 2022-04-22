d068b1bb4bb7deb43fc96026da9d339508727bab
BigDany1792 <dany1792dota@gmail.com>
2022-04-22 14:34:19 -0500

    style: fix betty style

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,15 +5,15 @@
  * @src: the memory that will be copied
  * @dest: the memory area destinated
  * @n: numbers of bytes that will be copied
- * Return: pointer to dest 
+ * Return: pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-        *(dest + i) = *(src + i);
-    
-    return (0);
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return (0);
 }
