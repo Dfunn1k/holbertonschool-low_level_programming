c0be3232f063c203678c77242e82e020f202dcec
BigDany1792 <dany1792dota@gmail.com>
2022-04-22 13:17:07 -0500

    ci: added function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ * @src: the memory that will be copied
+ * @dest: the memory area destinated
+ * @n: numbers of bytes that will be copied
+ * Return: pointer to dest 
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+        *(dest + i) = *(src + i);
+    
+    return (0);
+}
