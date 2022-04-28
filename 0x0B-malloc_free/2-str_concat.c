107a7d6a5fbcc90631235282e955fa134101fde9
BigDany1792 <dany1792dota@gmail.com>
2022-04-27 21:03:54 -0500

    ci: added function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: a pointer to strings concatenates
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, tmp, counts1 = 0, counts2 = 0;
+	char *cp;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	while (s1[counts1] != '\0')
+		counts1++;
+
+	while (s2[counts2] != '\0')
+		counts2++;
+
+	cp = malloc((counts1 + counts2 + 1) * sizeof(char));
+
+	if (cp == NULL)
+		return (NULL);
+
+	for (i = 0; i < counts1; i++)
+		cp[i] = s1[i];
+
+	tmp = i;
+
+	for (i = 0; i <= (counts2 + 1); i++)
+	{
+		if (i == counts2)
+			cp[tmp] = '\0';
+		else
+			cp[tmp] = s2[i];
+		tmp++;
+	}
+	return (cp);
+}
