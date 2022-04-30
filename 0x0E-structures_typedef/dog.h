a0a4b2e546a0a433d514abb71f5b6b33b831f426
BigDany1792 <dany1792dota@gmail.com>
2022-04-29 19:43:17 -0500

    ci: added header file and define a new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/**
+ * struct dog - Struct for dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
