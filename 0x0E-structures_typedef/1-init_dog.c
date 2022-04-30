7a2364c17518dacdef4d695a6bddcd6fe49d5884
BigDany1792 <dany1792dota@gmail.com>
2022-04-29 19:57:18 -0500

    ci: adde function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: pointer to struct
+ * @name: pointer to name
+ * @age:pointer to age
+ * @owner: pointer to owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,14 @@
  * @age: dog's age
  * @owner: dog's owner
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
