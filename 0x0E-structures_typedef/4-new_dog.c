bd4c65aa12f586583a702e533d173d7b1dcbb068
BigDany1792 <dany1792dota@gmail.com>
2022-04-29 23:25:27 -0500

    ci: added function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,48 @@
 #include "dog.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int x;
+
+	x = 0;
+	while (s[x] != '\0')
+		x++;
+	return (x);
+}
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
 
 /**
  * new_dog - that creates a new dog
@@ -17,9 +61,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = name;
+	new_dog->name = malloc(_strlen(name) + 1);
+
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = malloc(_strlen(owner) + 1);
+
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		free(new_dog->owner);
+		return (NULL);
+	}
+
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
