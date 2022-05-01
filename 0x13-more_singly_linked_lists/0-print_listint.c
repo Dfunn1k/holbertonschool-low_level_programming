d046e3cedf397def24385b9642398a4262f57e70
BigDany1792 <dany1792dota@gmail.com>
2022-05-01 13:59:06 -0500

    ci: added function that count the number of nodes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,14 +1,22 @@
 #include "lists.h"
+
 /**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: pointer to struct
- *
- * Return: The number of nodes
+ * print_listint - count the number of nodes
+ * @h: pointer to head
+ * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	if (h == NULL)
-		return (0);
-	printf("%d\n", h->n);
-	return (1 + print_listint(h->next));
+	listint_t *ptr;
+	size_t nodes;
+
+	ptr = h;
+
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+		nodes++;
+	}
+	return (nodes);
 }
