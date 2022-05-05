3d03af0805b2a6b1d3d79e1adcb08df348931783
BigDany1792 <dany1792dota@gmail.com>
2022-05-05 14:23:37 -0500

    ci: added function that add node at beginning in a dobly linked list2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node at beginning of a list
+ * @head: pointe to pointer to head
+ * @n: the value
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *ptr;
+
+	ptr = *head;
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (*head);
+	}
+	
+	new->next = ptr;
+	ptr->prev = new;
+	*head = new;
+
+	return (new);
+}
